b9f1549f199998c2bebbe49bc86803c622d33590
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-27 17:17:47 +0100

    updated readme.md and added comments in the code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 // 1 - Creo un array con la lista della spesa
 let list = ['uova' , 'pecorino', 'guanciale', 'pepe', 'pasta'];
 
-
-// Dichiarazione variabile contatore
-let i = 0;
-let list_print = '';
+// 3 - Link ul dell'html nello script
 let html_list = document.getElementById('shipping-list');
 
-// 4 - Creo il ciclo while
+// 4 -Dichiarazione variabile contatore
+let i = 0;
+
+// 5 - Dichiaro variabile di partenza della lista
+let list_print = '';
+
+// 6 - Inizializzo il ciclo while
 while(list[i]){
+    // 7 - Cambio la variabile vuota di list_print aggiungendo gli elementi dell'array
     list_print += list[i];
+    // 8 - Creo la costante li nello script
     const li = document.createElement('li');
+    // 9 - Stampo nella costante li la lista del ciclo while
     li.innerText = list[i];
-    console.log(li);
+    // 10 - Linko la costante li come figlio della lista ul presente nell'HTML
     html_list.appendChild(li);    
     i++;
+    console.log(li);
 }
 
 
--- readme.md
+++ readme.md
@@ -6,7 +6,13 @@ Consigli: - Ricordiamoci di inizializzare la variabile di contatore prima dell'i
 Buon lavoro a tutte e a tutti!!
 
 Svolgimento:
-1 - Creo un array con la lista della spesa
-2 - Creo un ul nel file html e gli do un id
-3 - Linko l'ul nello script js
-4 - Creo ciclo while
+1 - Creo un array con la lista della spesa;
+2 - Creo un ul nel file html e gli do un id;
+3 - Linko l'ul presente nell'html nello script js;
+4 - Dichiaro variabile contatore;
+5 - Dichiaro variabile di partenza della lista;
+6 - Inizializzo ciclo while;
+7 - Cambio la variabile vuota di list_print aggiungendo gli elementi dell'array;
+8 - Creo la costante li nello script;
+9 - Stampo nella costante li la lista del ciclo while
+10 - Linko la costante li come figlio della lista ul presente nell'HTML
