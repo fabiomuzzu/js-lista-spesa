5f7b602a0f026904ab7d674ce70640489fdc9296
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-27 13:37:55 +0100

    updated readme.md, html and script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// 1 - Creo un array con la lista della spesa
+const list = ['uova' , 'pecorino', 'guanciale', 'pepe', 'pasta'];
+
+// 3 - Link ul dell'HTML 
+const list_container = document.getElementById('shipping-list');
+
+// 4 - Creo il ciclo while
+while(){
+
+    i++;
+}
+
+
+// for(let i=0; i<list.length; i++){
+//     const li = document.createElement('li');
+//     li.innerText = list[i];
+
+//     list_container.appendChild(li);
+// }
